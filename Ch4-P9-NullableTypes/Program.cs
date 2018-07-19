04b5a10fcf2d8d773faccbc9f625622ff790cc1e
Ammar Innowi <ammar.shaukat@innowi.com>
2018-07-19 15:04:51 +0500

    Commit from ges

---
+++ Ch4-P9-NullableTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_P9_NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  " Nullable types in C#  ");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
